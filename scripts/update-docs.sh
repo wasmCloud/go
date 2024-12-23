4b3cfc22e8824f0a756562e49dc42c99b62997b6
Taylor Thomas <taylor@cosmonic.com>
2024-12-23 14:46:11 -0700

    feat(ci): Adds automatic doc PR creation for examples
    This adds a new pipeline that will automatically sync example READMEs to
    the docs repo via PR whenever there are changes
    
    Signed-off-by: Taylor Thomas <taylor@cosmonic.com>

---
+++ scripts/update-docs.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# If no arguments are passed, print help message and exit
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <docs_repo>"
+    exit 1
+fi
+
+docs_repo=$1
+
+# Get the top-level directory of the repository
+docs_dir="$(git rev-parse --show-toplevel)/examples"
+
+while IFS= read -r -d '' file
+do
+  # Get the relative path of the file from the repo root
+    rel_path=$(dirname "${file#"$docs_dir/"}")
+    # This uppercases the first letter of each word in the path so the sidebar is correct. We may
+    # change this in the future with some metadata in the docs repo
+    target_dir="$docs_repo/docs/Examples/Go/${rel_path^}"
+    # Create the target directory structure
+    mkdir -p "${target_dir}"
+    # Copy the file to the docs repo
+    cp "$file" "${target_dir}/index.md"
+done <  <(find "${docs_dir}" -name "README.md" -type f -print0)
+
+# Now check if there are any git changes in the docs repo
+if [[ -z "$(git -C "$docs_repo" status --porcelain)" ]]; then
+    echo "No changes detected in docs repo. Exiting..."
+    exit 0
+fi
+
+echo "Changes detected in docs repo after syncing"
+# If GITHUB_OUTPUT is set, write a variable to it. This is mostly here so people can run it locally
+# if desired and for testing.
+if [ -n "${GITHUB_OUTPUT:-}" ]; then
+    echo "docs_changed=true" >> "$GITHUB_OUTPUT"
+fi
+exit 0
